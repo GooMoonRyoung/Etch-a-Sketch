3ddb29a66523bbb6e468eccbccc6b65c82142338
GooMoonRyoung <andrew.truong25@gmail.com>
2023-04-23 15:59:51 -0400

    created functionality for user to choose grid size.

--- jsfile.js
+++ jsfile.js
@@ -5,7 +5,7 @@ I will add a hover event to the boxes that adds a transition/css class and when 
 
 const container = document.querySelector('#container');
 const reset = document.querySelector('#reset');
-const grid = document.querySelector('grid')
+const grid = document.querySelector('#grid')
 const xDefault = 16
 const yDefault = 16
 
@@ -23,20 +23,42 @@ function createGrid(yAxis, xAxis){
             currentRow.appendChild(box);
         }   
     }
+    addBoxEvents();
 }
 
-createGrid(yDefault, xDefault)
-//Adds a class to the boxes when they are hovered over and changes their colour over a transition period
-const boxes = document.querySelectorAll('.box');
-boxes.forEach(box => {
-    box.addEventListener('mouseover',function (e){
-        this.classList.add('activated')
-    });
+// Asks for
+grid.addEventListener('click', function(e){
+    while (container.hasChildNodes()){
+        container.firstChild.remove()
+    };
+    const newXAxis = Number(prompt('how many boxes should there be for the width?(The limit is 100)'))
+    if (newXAxis > 100){
+        newXAxis = xDefault
+        alert(`you can't input a number greater than 100`)
+    }
+    const newYAxis = Number(prompt('how many boxes should there be for the height?(The limit is 100)'))
+    if (newXAxis > 100){
+        newXAxis = xDefault
+        alert(`you can't input a number greater than 100`)
+    }
+    createGrid(newYAxis, newXAxis)
 })
 
+createGrid(yDefault, xDefault)
+
+//Adds a class to the boxes when they are hovered over and changes their colour over a transition period
+function addBoxEvents(){
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.addEventListener('mouseover',function (e){
+            this.classList.add('activated')
+        });
+    })
+}
 // Resets all the boxes to their original colour
 reset.addEventListener('click', function(e){
+    const boxes = document.querySelectorAll('.box');
     boxes.forEach(box => {
-        box.classList.remove('activated')
+        box.classList.remove('activated');
     })
 });
