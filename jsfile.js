3df06a356a204d70c95f525ecd9c054d16fcff66
GooMoonRyoung <andrew.truong25@gmail.com>
2023-04-23 13:24:13 -0400

    created the outline for the program within the js file and added some css classes

---
+++ jsfile.js
@@ -0,0 +1,4 @@
+/*I will try and generate a row div and within the row div I will go ahead and add 16 div boxes 
+I will add 16 of these rows so that and this will allow me to have the grid that I want
+I will add a hover event to the boxes that adds a transition/css class and when the mouse is no longer hovering it transition back to normal.
+*/
