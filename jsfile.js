3756e137a2eda05e64a1dc95f4f0e4f739560cd5
GooMoonRyoung <andrew.truong25@gmail.com>
2023-04-23 13:39:45 -0400

    attached the js and css files to the index. Created 16 rows with divs.

--- jsfile.js
+++ jsfile.js
@@ -2,3 +2,11 @@
 I will add 16 of these rows so that and this will allow me to have the grid that I want
 I will add a hover event to the boxes that adds a transition/css class and when the mouse is no longer hovering it transition back to normal.
 */
+
+const container = document.querySelector('#container');
+
+for ( i=0; i < 16; i++){
+    const row = document.createElement('div')
+    row.classList.add('row')
+    container.appendChild(row)
+}
